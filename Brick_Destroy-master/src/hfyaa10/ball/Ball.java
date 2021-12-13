50879ba5154b69d8c1c7aa7cd0f6ba9763ae49ee
rzqshm <sakalishpro@gmail.com>
2021-12-13 21:44:57 +0800

    rearranged project's package structure

---
+++ Brick_Destroy-master/src/hfyaa10/ball/Ball.java
@@ -0,0 +1,147 @@
+package hfyaa10.ball;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.geom.RectangularShape;
+
+/**
+ * Created by filippo on 04/09/16.
+ *
+ */
+abstract public class Ball {
+
+    private Shape ballFace;
+
+    private Point2D center;
+
+    private Point2D up;
+    private Point2D down;
+    private Point2D left;
+    private Point2D right;
+
+    private Color border;
+    private Color inner;
+
+    private int speedX;
+    private int speedY;
+
+    public Ball(Point2D center,int radiusA,int radiusB,Color inner,Color border){
+
+        this.center = center;
+        newLocation(); //ni
+        setPoints(radiusA, radiusB); //ni
+
+        ballFace = makeBall(center,radiusA,radiusB);
+
+        this.border = border;
+        this.inner  = inner;
+
+        setSpeed(0, 0); //ni
+
+    }
+
+    protected abstract Shape makeBall(Point2D center,int radiusA,int radiusB);
+
+    public void move(){
+        RectangularShape tmp = (RectangularShape) ballFace;
+        center.setLocation((center.getX() + speedX),(center.getY() + speedY));
+        double w = tmp.getWidth();
+        double h = tmp.getHeight();
+
+        tmp.setFrame((center.getX() -(w / 2)),(center.getY() - (h / 2)),w,h);
+        setPoints(w,h);
+
+
+        ballFace = tmp;
+    }
+
+    public void setSpeed(int x,int y){
+        speedX = x;
+        speedY = y;
+    }
+
+    public void setXSpeed(int s){
+        speedX = s;
+    }
+
+    public void setYSpeed(int s){
+        speedY = s;
+    }
+
+    public void reverseX(){
+        speedX *= -1;
+    }
+
+    public void reverseY(){
+        speedY *= -1;
+    }
+
+    public Color getBorderColor(){
+        return border;
+    }
+
+    public Color getInnerColor(){
+        return inner;
+    }
+
+    public Point2D getPosition(){
+        return center;
+    }
+
+    public Shape getBallFace(){
+        return ballFace;
+    }
+
+    // methods to enable access to private variable
+    public Point2D getUp() {  return up  ;}
+
+    public Point2D getDown() {
+        return down;
+    }
+
+    public Point2D getLeft() {
+        return left;
+    }
+
+    public Point2D getRight() {
+        return right;
+    }
+
+    public void moveTo(Point p){
+        center.setLocation(p);
+
+        RectangularShape tmp = (RectangularShape) ballFace;
+        double w = tmp.getWidth();
+        double h = tmp.getHeight();
+
+        tmp.setFrame((center.getX() -(w / 2)),(center.getY() - (h / 2)),w,h);
+        ballFace = tmp;
+    }
+
+    private void setPoints(double width,double height){
+        up.setLocation(center.getX(),center.getY()-(height / 2));
+        down.setLocation(center.getX(),center.getY()+(height / 2));
+
+        left.setLocation(center.getX()-(width / 2),center.getY());
+        right.setLocation(center.getX()+(width / 2),center.getY());
+    }
+
+    public int getSpeedX(){
+        return speedX;
+    }
+
+    public int getSpeedY(){
+        return speedY;
+    }
+
+    private void newLocation(){
+        up = new Point2D.Double();
+        down = new Point2D.Double();
+        left = new Point2D.Double();
+        right = new Point2D.Double();
+    }
+
+
+
+
+}
